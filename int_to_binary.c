8bea1d64034a52d80a0788c06ebd34724b1a8232
Matumbai Binale <binalematumbai@gmail.com>
2023-11-18 07:37:41 -0800

    Handle case: 0 passed to '%b'

--- int_to_binary.c
+++ int_to_binary.c
@@ -10,6 +10,13 @@ int int_to_binary(unsigned int number)
 	char buffer[BUFFSIZE];
 	int index, rem, i, count;
 
+	count = 0;
+	if (number == 0)
+	{
+		count += _putchar('0');
+		return (count);
+	}
+
 	index = 0;
 
 	while (number > 0)
@@ -22,7 +29,6 @@ int int_to_binary(unsigned int number)
 	}
 	buffer[index] = '\0';
 
-	count = 0;
 	/* Print buffer in reverse */
 	for (i = index - 1; i >= 0; i--)
 	{
